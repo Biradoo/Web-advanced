cfcade0f89a6b2ac0b9a946520e82729792e1fd1
Bersan Yonca <bersan2000@live.nl>
2024-12-08 22:13:02 +0100

    Different approach

---
+++ web-advanced/server/src/utils/jwtSecret.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt';
+
+const JWT_SECRET = 'yourSuperSecretKey'; // Move this to .env in production
+
+export const hashSecret = async () => bcrypt.hash(JWT_SECRET, 10);
+export const getSecret = async () => JWT_SECRET; // Simplify for now, can include advanced fetching if needed.
+export const compareSecrets = async (secret1, secret2) => bcrypt.compare(secret1, secret2);
+
+export default JWT_SECRET;
